fcb1ad3bdebce60ddc24f344f38961561790f56a
Huy Nguyen <huynguyen.kh@groovetechnology.com>
2021-06-23 14:48:22 +0700

    refactor code

--- wiki/models/article.py
+++ wiki/models/article.py
@@ -5,7 +5,6 @@ from django.contrib.contenttypes import fields
 from django.contrib.auth.models import User, Group
 from django.db.models.fields import GenericIPAddressField
 from django.db import models
-from django.utils.encoding import DjangoUnicodeDecodeError
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
